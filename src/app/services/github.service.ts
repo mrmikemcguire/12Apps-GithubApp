1d3e3932187a9dd7899e6eda43a595aa2759be94
Mike McGuire <MoonshotMike@gmail.com>
2016-12-23 06:55:21 -0700

    working on http service

---
+++ src/app/services/github.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator.map';
+
+@Injectable()
+
+export class GithubService{
+    private username: string;
+    private client_id = 'eb0d867c2c6417fef689';  //found at github.settings.developer
+    private client_secret = '584f79da48c222865b337f1c2b39783e71dd6fa3';
+
+    constructor(private _http: Http){
+        console.log("Github Service Ready...");
+        this.username = 'mrmikemcguire';
+    }
+
+    getUser(){
+        return this._http.get('http://api.github.com/users/' +
+            this.username +'?client_id=' + this.client_id + '&client_secret = '+this.client_secret + '.map(res => res.json());
+    }
+}
+
