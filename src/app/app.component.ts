b83442afe209f36131add0f0730c932fb45045a9
Mike McGuire <MoonshotMike@gmail.com>
2016-12-23 06:06:12 -0700

    File name change from the MacBook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Initial test from office Mac';
+  title = 'File name change test';
 }
